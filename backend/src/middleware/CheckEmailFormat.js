5758ece64208f06ca278da36e43488cebb273b7f
Emmanuel Chigwende <emmanuelchigwende2@gmail.com>
2026-02-22 17:12:38 +0200

    added email validation

---
+++ backend/src/middleware/CheckEmailFormat.js
@@ -0,0 +1,19 @@
+const hasEmailFormat  = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+
+
+function  TestEmail(email){
+    const error = []
+
+    if(!hasEmailFormat.test(email)){
+        error.push("wrong email format please check format")
+    }
+
+    if(error.lenght === 0){
+        return true
+    }
+    else{
+        return error
+    }
+}
+
+export default TestEmail
