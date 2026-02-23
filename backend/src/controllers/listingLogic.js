be46da726d621a6bd2231db53210b3552629e69d
Emmanuel Chigwende <emmanuelchigwende2@gmail.com>
2026-02-23 14:09:43 +0200

    working on newlisting function

--- backend/src/controllers/listingLogic.js
+++ backend/src/controllers/listingLogic.js
@@ -1,6 +1,7 @@
 import ListingModel from "../models/listingModel.js";
 import HandleImages from "../services/imageHandling.js";
 import image from "../middleware/multer.js";
+import e from "express";
 
 async function GetAllListings(req, res) {
   try {
@@ -20,7 +21,38 @@ async function GetAllListings(req, res) {
 }
 
 async function CreateListing(req, res) {
-  return res.json("still building endpoint");
+  try {
+    const newListing = req.body;
+
+    if (!newListing) {
+      res.status("no listing data found");
+    } else {
+      const imageBuffer = image(newListing.images);
+      if (imageBuffer.lenght === 0) {
+        return res.status(404).json({ message: "please provide images" });
+      } else {
+        const imageUrl = HandleImages(imageBuffer);
+        if (!imageUrl) {
+          return res
+            .status(400)
+            .json({ message: "failed to upload image to supabase" });
+        } else {
+          if(await ListingModel.create({
+            ...newListing,
+            owner: newListing.owner,
+            images: imageUrl,
+          })){
+            return res.status(200).json({message:"successfully created listing"})
+          }
+          else{
+            return res.status(400).json({message:"failed to create listing"})
+          }
+        }
+      }
+    }
+  } catch (err) {
+    console.log("failed to create listing", err);
+  }
 }
 
 async function DeleteListingByID(req, res) {
@@ -37,20 +69,22 @@ async function DeleteListingByID(req, res) {
   }
 }
 
-async function DeleteAllListings(req,res) {
-  try{
-    const Listings = await ListingModel.deleteMany()
+async function DeleteAllListings(req, res) {
+  try {
+    const Listings = await ListingModel.deleteMany();
 
-    if(Listings.deletedCount === 0){
-      return res.status(200).json({message:"no action taken: There are no listings yet"})
+    if (Listings.deletedCount === 0) {
+      return res
+        .status(200)
+        .json({ message: "no action taken: There are no listings yet" });
+    } else {
+      return res
+        .status(200)
+        .json({ message: "successfully deleted all listings" });
     }
-    else{
-      return res.status(200).json({message:"successfully deleted all listings"})
-    }
-  }
-  catch(err){
-    console.log("failed to delete listings",err)
+  } catch (err) {
+    console.log("failed to delete listings", err);
   }
 }
 
-export { GetAllListings, CreateListing ,DeleteListingByID,DeleteAllListings};
+export { GetAllListings, CreateListing, DeleteListingByID, DeleteAllListings };
