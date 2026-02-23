bc2cd7411b6680720cf79112669d7c1907da2d32
Emmanuel Chigwende <emmanuelchigwende2@gmail.com>
2026-02-23 17:23:54 +0200

    reminder note added

--- backend/src/services/imageHandling.js
+++ backend/src/services/imageHandling.js
@@ -1,7 +1,9 @@
 import supabase from "../config/supabaseClient.js";
 
 async function HandleImages(file) {
-  await supabase.storage.from("rethread").upload('/userUploadedImgaes',file,{contentType:"image.png"});
+  console.log("handle image fucntion was called")
+  const url =  await supabase.storage.from("rethread").upload('/userUploadedImgaes',file,{contentType:"image.png"});
+  // when returning return url.data
 }
 
 export default HandleImages;
